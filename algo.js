72c89ea943bda825159fcb736b8d8c5e48deb1c5
Tushar Nagpal <63884440+TusharSNagpal@users.noreply.github.com>
2022-09-13 14:20:31 +0530

    Create algo.js

---
+++ algo.js
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GAME | Tushar Nagpal</title>
+
+    <link rel="stylesheet" href="/styles.css">
+    <link rel="shortcut icon" href="Images/icon.png">
+
+</head>
+<body>
+   
+    <nav class="navigation container">
+        <div class="nav-this">Tushar Nagpal</div>
+        <div class="nav-this-small">&lt;developer /&gt; </div>
+        
+        <ul class="list_non-bullet nav-pills">
+            <li class="list_item-inline">
+                <a href="index.html" class="link">HOME</a>
+            </li>
+            <li class="list_item-inline">
+                <a href="projects.html" class="link">PROJECTS</a>
+            </li>
+            <li class="list_item-inline">
+                <a href="resume.html" class="link">RESUME</a>
+            </li>
+            <li class="list_item-inline link-active">
+                <a href="work.html" class="link">WORK</a>
+            </li>
+        </ul>
+    </nav>
+    <br>
+
+    <header class="hero">
+        <img src= "images/leapyear.svg" class="hero-image"/>
+        <h1 class="hero-header">ALGO QUESTION NUMBERS..</h1>
+    </header>
+    
+    <hr>
+
+    <div class="container-center">
+
+    <p id="q1">ENTER YOUR ROLL NUMBER (LAST 3 DIGITS): </p>
+    <textarea id="a1" placeholder="for example: MT2022001 -> 001"></textarea>
+
+    <br>
+    <br>
+
+    <button id="button-resp" class="primary-link">CHECK</button>
+    <br>
+    <br>
+    <div class="o">
+    <p id="output1"></p>
+    <p class="container-center" id="result1"> </p>
+    </div>
+</div>
+    <hr>
+
+    <section class="section offwhite">
+
+        
+        <div class="cen container-center">
+            <h1><u>AUTHOR:</u></h1>
+           <img src="/images/t.jpg" class="small" />
+           <p><span class="lang">TUSHAR NAGPAL</span></p>
+        </div>
+        <br> 
+    </section>
+
+    
+    <footer class="footer">
+        <div class="footer-header">We are social. Lets Connect!</div>
+        <br>
+
+        <ul class="social-links list_non-bullet">
+            <li class="list_item-inline"> <a class="link primary-link" href="https://github.com/TusharSNagpal"><img src= "images/GitHub-Mark-120px-plus.png" class="logo"/></a></li>
+            <li class="list_item-inline"> <a class="link primary-link" href="https://www.instagram.com/tushar_s_nagpal/"><img src= "images/OIP.png" class="small"/></a></li>
+            <li class="list_item-inline"> <a class="link secondary-link" href="https://www.facebook.com/tushar.nagpal.1420/" ><img src= "images/fb.png" class="small"/></a></li>
+            <li class="list_item-inline"> <a class="link secondary-link" href="https://www.linkedin.com/in/tushar-nagpal-aa76b91a1/" ><img src= "images/LI-In-Bug.png" class="small"/></a></li>
+            <li class="list_item-inline"> <a class="link secondary-link" href="https://twitter.com/tusharsnagpal1" ><img src= "images/2021 twitter.png" class="small"/></a></li>
+        </ul>
+    </footer>
+
+    <script src="algo.js" type="text/javascript"></script>
+</body>
+</html>
